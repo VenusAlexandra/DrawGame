b1e7c630d84b90c04091f1b6a91332f57a373aaf
VenusAlexandra <119280107+VenusAlexandra@users.noreply.github.com>
2023-02-11 09:59:37 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function whoIsWinner() {
+  var randomNumber1 = Math.random() * 6 + 1;
+  var randomNumber1 = Math.floor(randomNumber1);
+
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+  var randomNumber2 = Math.random() * 6 + 1;
+  var randomNumber2 = Math.floor(randomNumber2);
+
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins! 🚩";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+  } else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+}
+
+whoIsWinner();
